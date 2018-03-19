b0b111db247dd7dbbfacd37c4dad549fe12b2c57
Aaron Caldwell <aaron.caldwell@noaa.gov>
2018-03-19 13:22:15 -0600

    Init geosamples app

---
+++ src/main/java/cmg/geosamples/geosamples/GeosamplesApplication.java
@@ -0,0 +1,12 @@
+package cmg.geosamples.geosamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeosamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeosamplesApplication.class, args);
+	}
+}
