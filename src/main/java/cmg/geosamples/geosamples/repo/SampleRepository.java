7e1d9d16a69740f0a9d79c056d86c7578e3011c1
Aaron Caldwell <aaron.caldwell@noaa.gov>
2018-03-26 13:51:13 -0600

    Start customize bindings. Controlling for case and water depths

--- src/main/java/cmg/geosamples/geosamples/repo/SampleRepository.java
+++ src/main/java/cmg/geosamples/geosamples/repo/SampleRepository.java
@@ -1,9 +1,23 @@
 package cmg.geosamples.geosamples.repo;
 
+import cmg.geosamples.geosamples.domain.QSample;
 import cmg.geosamples.geosamples.domain.Sample;
+import com.querydsl.core.types.dsl.StringPath;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 
 public interface SampleRepository extends JpaRepository<Sample, Integer>,
-  QuerydslPredicateExecutor<Sample> {
+  QuerydslPredicateExecutor<Sample>, QuerydslBinderCustomizer<QSample> {
+
+  @Override
+  default public void customize(QuerydslBindings bindings, QSample sample) {
+    bindings.bind(String.class).first(
+      (StringPath path, String value) -> path.containsIgnoreCase(value));
+    bindings.bind(sample.water_depth).first((path, value) ->
+      path.goe(value));
+    bindings.bind(sample.end_water_depth).first((path, value) ->
+      path.loe(value));
+  }
 }
